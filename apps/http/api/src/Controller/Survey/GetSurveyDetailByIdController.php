202a5431c8fb5bc8ce0ba789c42dacd9e96ce189
viher3 <viher_3@hotmail.com>
2022-08-20 22:15:50 +0200

    Creates get survey detail controller

---
+++ apps/http/api/src/Controller/Survey/GetSurveyDetailByIdController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SurveySystem\Apps\Api\Controller\Survey;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class GetSurveyDetailByIdController extends AbstractController
+{
+    public function __invoke()
+    {
+
+    }
+}
