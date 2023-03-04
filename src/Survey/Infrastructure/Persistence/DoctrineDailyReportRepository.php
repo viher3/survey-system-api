8054baa4a75fb5513806ffec1f53dd26aaa969aa
viher3 <viher_3@hotmail.com>
2023-03-04 17:56:01 +0100

    Updates DoctrineDailyReportRepository

--- src/Survey/Infrastructure/Persistence/DoctrineDailyReportRepository.php
+++ src/Survey/Infrastructure/Persistence/DoctrineDailyReportRepository.php
@@ -20,7 +20,7 @@ class DoctrineDailyReportRepository extends DoctrineRepository implements DailyR
                              ->createQueryBuilder('q')
                              ->select('q.id, q.questionId, q.date, q.average, q.mode, qst.question, qst.position as questionPosition')
                              ->innerJoin(SurveyQuestion::class, 'qst', 'WITH', 'qst.id = q.questionId')
-                             ->orderBy('q.createdAt', 'DESC');
+                             ->orderBy('q.date', 'ASC');
 
         if(!empty($filters['surveyId'])){
             $queryBuilder->andWhere('q.surveyId = :surveyId')->setParameter('surveyId', $filters['surveyId']);
